4dc0eac262db557728a217cce49bd4d461d5d424
Radovan Marjanovic <marjanovic.radovan@gmail.com>
2020-06-08 12:22:18 +0200

    registration and login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  VALID_PASSWORD_REGEX = /\A(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/x
+
+  # for bcript gem
+  has_secure_password
+
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password,
+            length: { minimum: 6 },
+            if: -> { new_record? || !password.nil? }
+  validates_format_of :password, with: VALID_PASSWORD_REGEX, message: 'Complexity requirement not met. Please use: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
 end
